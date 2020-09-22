9242555d4f06958b54cc48171c631fa9b6289147
shafayat <shafayat18@gmail.com>
2020-09-22 14:22:18 +0600

    firebase hosting complete

---
+++ build/precache-manifest.fcd92c0f117626f81ba60203c9466c09.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9931439a808f8d3a42975f2f83f535a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "00517bd288bb0b054c6e",
+    "url": "/static/css/main.358c5e51.chunk.css"
+  },
+  {
+    "revision": "84ca1bcc39ee3afd52d2",
+    "url": "/static/js/2.884874eb.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/static/js/2.884874eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00517bd288bb0b054c6e",
+    "url": "/static/js/main.98a5c6cb.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "bf0ce9ae8f3d3bfb6adc309f522af1fe",
+    "url": "/static/media/1.bf0ce9ae.png"
+  },
+  {
+    "revision": "0d1e50bc75ccad7a3245adf9793e7874",
+    "url": "/static/media/2.0d1e50bc.png"
+  },
+  {
+    "revision": "7a816f2ed7a3a828950d93e383fb7f54",
+    "url": "/static/media/3.7a816f2e.png"
+  },
+  {
+    "revision": "d2db19f4510c91b3ce921cb65841fe01",
+    "url": "/static/media/Rectangle.d2db19f4.jpg"
+  },
+  {
+    "revision": "7322377073fe843727066490a5e62092",
+    "url": "/static/media/Sajek.73223770.png"
+  },
+  {
+    "revision": "1cff006ac872409a6ce087916b364913",
+    "url": "/static/media/Sreemongol.1cff006a.png"
+  },
+  {
+    "revision": "015d2c10b3084491ecec825ba3580aac",
+    "url": "/static/media/sundorbon.015d2c10.png"
+  }
+]);
